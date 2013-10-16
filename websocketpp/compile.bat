e5999e8f71e1b9bb63103a02c4ba16b8d0f7ecd4
Daniele Pallastrelli <daniele.pallastrelli@gmail.com>
2013-10-16 15:33:27 +0200

    Server send msg spontaneusly

--- websocketpp/compile.bat
+++ websocketpp/compile.bat
@@ -1,1 +1,4 @@
-cl /EHa /DWIN32 /I..\..\..\websocket\websocketpp-master /I..\..\..\boost_1_51_0 echo_server.cpp /link /LIBPATH:..\..\..\boost_1_51_0\stage\lib
+SET boost_path=..\..\..\boost_1_51_0\
+SET websocketpp_path=..\..\..\websocket\websocketpp-master
+
+cl /EHa /DWIN32 /I%websocketpp_path% /I%boost_path% echo_server.cpp /link /LIBPATH:%boost_path%stage\lib
--- websocketpp/echo_server.cpp
+++ websocketpp/echo_server.cpp
@@ -1,7 +1,9 @@
 #include <websocketpp/config/asio_no_tls.hpp>
-
 #include <websocketpp/server.hpp>
 
+#include <functional>
+#include <set>
+#include <memory>
 #include <iostream>
 
 typedef websocketpp::server<websocketpp::config::asio> server;
@@ -9,52 +11,98 @@ typedef websocketpp::server<websocketpp::config::asio> server;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
+using websocketpp::connection_hdl;
 
 // pull out the type of messages sent by our config
 typedef server::message_ptr message_ptr;
 
-// Define a callback to handle incoming messages
-void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
-    std::cout << "on_message called with hdl: " << hdl.lock().get()
-              << " and message: " << msg->get_payload()
-              << std::endl;
+class MyServer
+{
+    public:
 
-    try {
-        s->send(hdl, msg->get_payload(), msg->get_opcode());
-    } catch (const websocketpp::lib::error_code& e) {
-        std::cout << "Echo failed because: " << e
-                  << "(" << e.message() << ")" << std::endl;
-    }
-}
+        // Define a callback to handle incoming messages
+        void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
+            std::cout << "on_message called with hdl: " << hdl.lock().get()
+                      << " and message: " << msg->get_payload()
+                      << std::endl;
+
+            try {
+                s->send(hdl, msg->get_payload(), msg->get_opcode());
+            } catch (const websocketpp::lib::error_code& e) {
+                std::cout << "Echo failed because: " << e
+                          << "(" << e.message() << ")" << std::endl;
+            }
+        }
+
+        void on_timer(const websocketpp::lib::error_code & ec) {
+            std::cout << "Timer expired: sending message" << std::endl;
+            std::stringstream ss;
+            ss << count++;
+            for (auto it : connections)
+                echo_server.send( it, ss.str(), websocketpp::frame::opcode::text );
+            timer = echo_server.set_timer(1000, bind(&MyServer::on_timer,this,_1));
+        }
+        
+        void on_open(connection_hdl hdl)
+        {
+            std::cout << "someone connected" << std::endl;
+            connections.insert(hdl);
+        }
+    
+        void on_close(connection_hdl hdl)
+        {
+            std::cout << "someone disconnected" << std::endl;
+            connections.erase(hdl);
+        }
+        
+        MyServer() : count( 0 )
+        {
+            try
+            {
+                // Set logging settings
+                echo_server.set_access_channels(websocketpp::log::alevel::all);
+                echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+                // Initialize ASIO
+                echo_server.init_asio();
+                
+                timer = echo_server.set_timer(1000, bind(&MyServer::on_timer,this,_1));
+
+                // Register our handlers
+                echo_server.set_open_handler(bind(&MyServer::on_open,this,_1));
+                echo_server.set_close_handler(bind(&MyServer::on_close,this,_1));
+                echo_server.set_message_handler(bind(&MyServer::on_message,this,&echo_server,::_1,::_2));
+
+                // Listen on port 9002
+                echo_server.listen(9002);
+
+                // Start the server accept loop
+                echo_server.start_accept();
+            } catch (const std::exception & e) {
+                std::cout << e.what() << std::endl;
+            } catch (websocketpp::lib::error_code e) {
+                std::cout << e.message() << std::endl;
+            } catch (...) {
+                std::cout << "other exception" << std::endl;
+            }
+        }
+        
+        void Run()
+        {
+            // Start the ASIO io_service run loop
+            echo_server.run();
+        }
+
+    private:
+    
+        server echo_server;
+        server::timer_ptr timer;
+        size_t count;
+        typedef std::set<connection_hdl/*,std::owner_less<connection_hdl>*/ > con_list;
+        con_list connections;
+};
 
 int main() {
-	// Create a server endpoint
-    server echo_server;
-
-	try {
-        // Set logging settings
-        echo_server.set_access_channels(websocketpp::log::alevel::all);
-        echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
-
-        // Initialize ASIO
-        echo_server.init_asio();
-
-        // Register our message handler
-        echo_server.set_message_handler(bind(&on_message,&echo_server,::_1,::_2));
-
-        // Listen on port 9002
-        echo_server.listen(9002);
-
-        // Start the server accept loop
-        echo_server.start_accept();
-
-	    // Start the ASIO io_service run loop
-        echo_server.run();
-    } catch (const std::exception & e) {
-        std::cout << e.what() << std::endl;
-    } catch (websocketpp::lib::error_code e) {
-        std::cout << e.message() << std::endl;
-    } catch (...) {
-        std::cout << "other exception" << std::endl;
-    }
+    MyServer s;
+    s.Run();
 }
