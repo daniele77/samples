cf6616d6d30725fb6514f5e56dd264379713ad33
Daniele Pallastrelli <daniele.pallastrelli@gmail.com>
2015-04-05 21:06:36 +0200

    Aggiunto osservabile a esempio knokout

--- knokout/main.js
+++ knokout/main.js
@@ -1,11 +1,19 @@
-$( document ).ready(function() {
-    console.log( 'Hello, console!' )
+"use strict";
+
+$( document ).ready( function() {
+
+    console.log( 'Hello, console!' );
     
     var myViewModel = {
-        personName: 'Bob',
-        personAge: 123
+        personName: 'Bob', // la UI non viene aggiornata sui cambiamenti
+        personAge: ko.observable( 17 ) // la UI viene aggiornata sui cambiamenti
     };
 
     ko.applyBindings(myViewModel);
 
+    // modifica un osservabile dopo 5 secondi
+    setTimeout(function() {
+        myViewModel.personAge( 18 );
+    }, 5000);
+    
 });
